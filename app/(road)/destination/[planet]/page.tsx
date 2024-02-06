7ae2a9ca7e8af505f25afd3fe90b5e3d439c3214
HeiPavel <112711905HeiPavel@users.noreply.github.com>
2024-02-07 00:26:45 +0200

    Placeholder for destination images deleted

--- app/(road)/destination/[planet]/page.tsx
+++ app/(road)/destination/[planet]/page.tsx
@@ -6,7 +6,6 @@ import styles from './planet.module.scss';
 import globalStyles from '../../../styles/global.module.scss';
 import { data, navigation } from "@/app/util/data";
 import { imageLoader } from "@/app/util/imageLoader";
-import { planetPlaceholder } from "@/app/util/urlData";
 
 type Params = {
     params: {
@@ -32,8 +31,6 @@ export default function Page({params} : Params) {
                         sizes="(max-width: 480px) 100vw, (max-width: 1024px) 50vw, 33vw"
                         priority={true}
                         quality={100}
-                        placeholder="blur"
-                        blurDataURL={planetPlaceholder}
                     />
                 </div>
                 <div className={styles.object_description}>
